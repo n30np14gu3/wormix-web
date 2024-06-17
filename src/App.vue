551ed722cec2327788c0c95c9fad8bd9b9c34991
n30np1gu3 <darknetsoft@yandex.ru>
2024-06-17 14:06:30 +0300

    Full push

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,45 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="page-layout">
+    <vk-header></vk-header>
+  </div>
+  <div style="padding: 10px">
+    <div class="button-blue">
+      <button>Войти</button>
+    </div>
+  </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  font-family: tahoma, arial, verdana, sans-serif, Lucida Sans;
+  font-size: 11px;
+  background: white;
+}
+.button-blue {
+  padding: 5px 0;
+  border-radius: 0;
+}
+.button-blue button{
   text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+  padding: 5px 40px;
+  border-radius: 2px;
+  border: solid 1px rgb(69, 104, 141);
+  box-shadow: inset 0 0 0 1px rgba(255, 255, 255, 0.15);
+  background: linear-gradient(180deg, #698cb1 10%, #51769e 100%);
+  color: white;
+  text-shadow:  0 1px 0 rgba(0, 0, 0, 0.34);
 }
 </style>
+<script>
+import VkHeader  from "@/components/old-vk/header/VkHeader.vue";
+
+export default {
+  components: {
+    VkHeader
+
+  }
+}
+</script>
---
+++ src/components/old-vk/header/VkHeader.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="page-header">
+    <!--Да, раньше было так :)-->
+    <div class="back"></div>
+    <div class="left"></div>
+    <div class="right"></div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+div {
+  display: block;
+  unicode-bidi: isolate;
+}
+
+#page-header {
+  height: 40px;
+}
+
+#page-header .back {
+  position: absolute;
+  width: 100%;
+  height: 40px;
+  background: #4E729A url("../../../resources/old-vk/images/hat.gif") 0px -43px repeat-x;
+}
+
+#page-header .left {
+  position: absolute;
+  left: 0px;
+  width: 134px;
+  height: 40px;
+  background: #4E729A url("../../../resources/old-vk/images/hat.gif") 0px -1px;
+}
+
+#page-header .right {
+  position: absolute;
+  right: 0;
+  width: 10px;
+  border-right: 1px solid white;
+  height: 40px;
+  background: #4E729A url("../../../resources/old-vk/images/hat.gif") 100% -1px;
+}
+</style>
